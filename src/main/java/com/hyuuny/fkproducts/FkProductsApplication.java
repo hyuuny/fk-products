2135aba35ce213a333880931e1414c5dfe8e5e5c
hyuuny <shyune@knou.ac.kr>
2025-02-16 12:00:03 +0900

    feat: init project

---
+++ src/main/java/com/hyuuny/fkproducts/FkProductsApplication.java
@@ -0,0 +1,13 @@
+package com.hyuuny.fkproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkProductsApplication.class, args);
+	}
+
+}
