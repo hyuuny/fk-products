47dad3a08d88dc67a0de5cfad9b7d11fd9ee98d6
hyuuny <shyune@knou.ac.kr>
2025-02-21 17:12:25 +0900

    test: 상품 목록 조회 및 검색 API 테스트 구현

--- src/test/java/com/hyuuny/fkproducts/products/controller/ProductControllerTest.java
+++ src/test/java/com/hyuuny/fkproducts/products/controller/ProductControllerTest.java
@@ -4,6 +4,7 @@ import com.hyuuny.fkproducts.BaseIntegrationTest;
 import com.hyuuny.fkproducts.products.domain.ProductEntity;
 import com.hyuuny.fkproducts.products.domain.ProductRepository;
 import com.hyuuny.fkproducts.products.service.ProductDto;
+import com.hyuuny.fkproducts.products.service.ProductService;
 import com.hyuuny.fkproducts.support.error.ErrorType;
 import com.hyuuny.fkproducts.support.response.ResultType;
 import org.junit.jupiter.api.AfterEach;
@@ -14,6 +15,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -24,6 +27,9 @@ class ProductControllerTest extends BaseIntegrationTest {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private ProductService productService;
 
     @AfterEach
     void tearDown() {
@@ -166,6 +172,46 @@ class ProductControllerTest extends BaseIntegrationTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("error.message").value(ErrorType.PRODUCT_NOTFOUND.getMessage()))
                 .andExpect(jsonPath("error.data").value("상품을 찾을 수 없습니다 id:" + invalidId));
+    }
+
+    @DisplayName("상품 목록을 조회 및 검색할 수 있다")
+    @Test
+    void getProducts() throws Exception {
+        List<ProductDto.Create> products = Arrays.asList(
+                new ProductDto.Create("사과", 2000L, 3000L, "맛있는 사과예요"),
+                new ProductDto.Create("포도", 3000L, 3000L, "맛있는 포도예요"),
+                new ProductDto.Create("수박", 4000L, 3000L, "맛있는 수박이예요"),
+                new ProductDto.Create("딸기", 5000L, 3000L, "맛있는 딸기예요"),
+                new ProductDto.Create("바나나", 3000L, 3000L, "맛있는 바나나예요"),
+                new ProductDto.Create("참외", 4000L, 3000L, "맛있는 참외예요"),
+                new ProductDto.Create("아메리카노", 1500L, 3000L, "맛있는 아메리카노예요"),
+                new ProductDto.Create("카페라떼", 2000L, 3000L, "맛있는 카페라떼예요"),
+                new ProductDto.Create("핫초코", 2000L, 3000L, "맛있는 핫초코예요"),
+                new ProductDto.Create("카푸치노", 25000L, 3000L, "맛있는 카푸치노예요")
+        );
+        List<ProductDto.Response> savedProducts = products.stream()
+                .map(productService::createProduct)
+                .toList();
+
+        mockMvc.perform(get("/api/v1/products")
+                        .header(HttpHeaders.AUTHORIZATION, this.getBearerToken(CUSTOMER_EMAIL, CUSTOMER_PASSWORD))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("data.content[0].name").value(savedProducts.get(9).name()))
+                .andExpect(jsonPath("data.content[1].name").value(savedProducts.get(8).name()))
+                .andExpect(jsonPath("data.content[2].name").value(savedProducts.get(7).name()))
+                .andExpect(jsonPath("data.content[3].name").value(savedProducts.get(6).name()))
+                .andExpect(jsonPath("data.content[4].name").value(savedProducts.get(5).name()))
+                .andExpect(jsonPath("data.content[5].name").value(savedProducts.get(4).name()))
+                .andExpect(jsonPath("data.content[6].name").value(savedProducts.get(3).name()))
+                .andExpect(jsonPath("data.content[7].name").value(savedProducts.get(2).name()))
+                .andExpect(jsonPath("data.content[8].name").value(savedProducts.get(1).name()))
+                .andExpect(jsonPath("data.content[9].name").value(savedProducts.get(0).name()))
+                .andExpect(jsonPath("data.totalPages").value(1))
+                .andExpect(jsonPath("data.totalElements").value(10))
+                .andExpect(jsonPath("data.last").value(true));
     }
 
     private ProductEntity generateProduct() {
